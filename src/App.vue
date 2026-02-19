f02e7eec42cfe07099a7680e039eb0e613d6b323
Ivan <ivantrubar@campus.monlau.com>
2026-02-19 16:42:36 +0100

    v1.0

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app-container">
+    <TopBar />
+    <div class="main-content">
+      <FurnitureSidebar />
+      <RoomScene />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TopBar from './components/TopBar.vue'
+import FurnitureSidebar from './components/FurnitureSidebar.vue'
+import RoomScene from './components/RoomScene.vue'
+</script>
+
+<style scoped>
+.app-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: #f0f0f0;
+}
+
+.main-content {
+  flex: 1;
+  display: flex;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <App />
+  </div>
+</template>
+
+<script setup>
+import App from '../App.vue'
+</script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
